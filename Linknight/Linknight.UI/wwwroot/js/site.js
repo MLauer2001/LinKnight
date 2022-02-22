269940d4f1877868dd61bd9f78c28a6e53ba4b55
Elizabeth Richardson <200136420@fvtc.edu>
2022-02-22 17:16:09 -0600

    Updated current nav, nav-menu2 added, js added for active button to show and hide nav-menu2

--- Linknight/Linknight.UI/wwwroot/js/site.js
+++ Linknight/Linknight.UI/wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(function () {
+    $(".nav-menu2").hide();
+
+    $(".nav-reveal").click(function () {
+        $(".nav-menu2").toggle(); // testing -> Test completed, text will now show toggling when menu button clicked.
+        //further testing to be completed to have menu item appear below original nav bar -> WIP
+        // alert("OMG IT WORKS!!!"); -- it does in fact work
+    })
+
+})
