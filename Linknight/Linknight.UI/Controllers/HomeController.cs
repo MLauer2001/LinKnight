34cb3fd2688577fbf07da2ce8c2920acca5c2a07
Elizabeth Richardson <200136420@fvtc.edu>
2022-02-26 16:37:04 -0600

    Added additional pages. Updated HomeController. Fix admin auth in layout.cshtml.

--- Linknight/Linknight.UI/Controllers/HomeController.cs
+++ Linknight/Linknight.UI/Controllers/HomeController.cs
@@ -28,6 +28,21 @@ namespace Linknight.UI.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult Setting()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
