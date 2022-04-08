22269e51758b898fc72197337fb75f590a885bae
Matthew Lauer <300055427@fvtc.edu>
2022-04-08 13:32:33 -0500

    Added Users

---
+++ Linknight/Linknight.DB/Post-Deploy/DefaultData/Users.sql
@@ -0,0 +1,7 @@
+﻿BEGIN
+	INSERT INTO tblUser (Id, Username, FirstName, LastName, Password)
+	VALUES
+	(NEWID(), 'User1', 'John', 'Smith', 'jsmith'),
+	(NEWID(), 'User2', 'Sarah', 'Baker', 'sbaker'),
+	(NEWID(), 'User3', 'Robert', 'Tanner', 'rtanner')
+END
