96602701812e618632d657a9cf67fb151a2b6261
Zachary Dorschner <300065059@fvtc.edu>
2022-04-23 16:47:58 -0500

    wonky video

---
+++ Linknight/Linknight.UI/wwwroot/js/Video/video.js
@@ -0,0 +1,36 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$(function () {
+
+    var changelink = "";
+    $("#select").click(function () {
+        alert("Is showing");
+        ShowBox();
+    });
+    $("#confirmurl").click(function () {
+        changelink = $("#urltextbox").val();
+        var texturl = $("#video").text(changelink)
+        //var url = changelink.split("v=")[1].substring(0, 11);
+        //$('#video').attr('src', "https://www.youtube.com/embed/" + url);
+
+        alert(texturl);
+        HideBox();
+    });
+
+    
+
+
+
+    function ShowBox() {
+        $("#textbox").show();
+        //alert("Div is showing");
+    }
+    function HideBox() {
+        $("#textbox").hide();
+        //alert("Div is showing");
+    }
+    
+
+})
