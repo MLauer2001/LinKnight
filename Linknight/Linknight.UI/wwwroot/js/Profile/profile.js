0b95b8f53c84f6f954af65a0e70714db5a5ee7bb
Elizabeth Richardson <200136420@fvtc.edu>
2022-04-23 12:49:05 -0500

    JavaScript for profile page separated from main site javascript. Added Reset functionality for profile page Avatars. Added start for Confirm Avatar functionality.

---
+++ Linknight/Linknight.UI/wwwroot/js/Profile/profile.js
@@ -0,0 +1,99 @@
+﻿// ALL CODE FOR PROFILE PAGE
+
+$(function () {
+
+    // HEAD CLICK
+    var clickhead = 1;
+    var headId;
+
+    $("#headBtnLeft").click(function () {
+        headId = clickhead;
+        if (clickhead > 1) {
+            //alert(clickhead);
+            clickhead--;
+            headId--;
+            $('#imgChangeHead').attr('src', '/Resources/Avatars/Head/Base/Hat' + headId + 'a.png')
+
+        } else {
+            //alert("Click Counter is less than or equal to 0")
+            clickhead = 4;
+            headId = 4;
+            $('#imgChangeHead').attr('src', '/Resources/Avatars/Head/Base/Hat' + headId + 'a.png')
+
+        }
+    });
+
+    $("#headBtnRight").click(function () {
+        headId = clickhead;
+        if (clickhead < 4) {
+            clickhead++;
+            headId++;
+            $('#imgChangeHead').attr('src', '/Resources/Avatars/Head/Base/Hat' + headId + 'a.png')
+
+        } else {
+            clickhead = 1;
+            headId = 1;
+            $('#imgChangeHead').attr('src', '/Resources/Avatars/Head/Base/Hat' + headId + 'a.png')
+
+        }
+    });
+
+    //BODY CLICK
+    var clickbody = 1;
+    var bodyId;
+
+    $("#bodyBtnLeft").click(function () {
+        bodyId = clickbody;
+        if (clickbody > 1) {
+            clickbody--;
+            bodyId--;
+            $('#imgChangeBody').attr('src', '/Resources/Avatars/Body/Base/Body' + bodyId + 'a.png')
+
+        } else {
+            clickbody = 4;
+            bodyId = 4;
+            $('#imgChangeBody').attr('src', '/Resources/Avatars/Body/Base/Body' + bodyId + 'a.png')
+        }
+    });
+
+    $("#bodyBtnRight").click(function () {
+        bodyId = clickbody;
+        if (clickbody < 4) {
+            clickbody++;
+            bodyId++;
+            $('#imgChangeBody').attr('src', '/Resources/Avatars/Body/Base/Body' + bodyId + 'a.png')
+
+        } else {
+            clickbody = 1;
+            bodyId = 1;
+            $('#imgChangeBody').attr('src', '/Resources/Avatars/Body/Base/Body' + bodyId + 'a.png')
+
+        }
+    });
+
+    // RESET AVATAR
+
+    $("#resetAvatar").click(function (){
+        // alert("button clicked"); --test completed
+        clickhead = 1,
+        headId = 1,
+        clickbody = 1,
+        bodyId = 1;
+        $('#imgChangeHead').attr('src', '/Resources/Avatars/Head/Base/Hat' + headId + 'a.png')
+        $('#imgChangeBody').attr('src', '/Resources/Avatars/Body/Base/Body' + bodyId + 'a.png')
+    });
+
+    // CONFIRM/DISPLAY AVATAR
+
+    $("#confirmAvatar").click(function () {
+        //confirm for undefined
+        if (headId == undefined || bodyId == undefined) {
+            // This would only be the case if the person confirmed their avatar immediately as a head and body number have not yet been defined.
+            headId = 1;
+            bodyId = 1;
+        }
+
+        alert("Head = " + headId + " Body = " + bodyId);
+
+    });
+})
